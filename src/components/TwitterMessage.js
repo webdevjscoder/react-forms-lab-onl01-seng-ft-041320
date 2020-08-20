4eac88db5910a292dd9782446e61669b8d45a19a
Juan Cobian <cobianjuan19@gmail.com>
2020-08-20 17:35:58 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,16 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      macChars: 280
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={} />
       </div>
     );
   }
